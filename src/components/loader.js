ba37b7f2f2351af542b4689c5ef8e65afe5414c8
rmurdoc90 <rmurdoc90@gmail.com>
2022-07-15 13:52:29 -0700

    loader working properly

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,11 +3,22 @@ import { useEffect } from "react"
 
 function Loader() {
   function loaderFade() {
-    let wrapper = document.getElementById("loader").style
-    console.log(wrapper)
-    setTimeout(() => {}, 3000)
-  }
+    setTimeout(() => {
+      let wrapper = document.getElementById("loader")
+      let count = 100
+      setInterval(opacChange, 10)
 
+      function opacChange() {
+        if (count >= 0) {
+          console.log(`In loop ${count}`)
+          wrapper.style.filter = `opacity(${count}%)`
+          count--
+        } else {
+          clearInterval(count)
+        }
+      }
+    }, 3000)
+  }
   useEffect(() => {
     loaderFade()
   })
