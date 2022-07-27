9b10333480659e43f2d8d1eaf306aa4805b196da
rmurdoc90 <rmurdoc90@gmail.com>
2022-07-27 16:29:08 -0700

    changing nav and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,8 @@
 import React from "react"
 
 function footer() {
-  return <div className="bg-slate-700">footer</div>
+  return
+  ;<div className="bg-slate-700 h-12 p-4"></div>
 }
 
 export default footer
--- src/components/nav.js
+++ src/components/nav.js
@@ -24,12 +24,6 @@ function nav() {
           Experience
         </button>
         <button
-          onClick={() => scrollTo("#socials")}
-          className="bg-slate-500 hover:bg-green-600 py-2 px-4 rounded text-sm"
-        >
-          Socials
-        </button>
-        <button
           onClick={() => scrollTo("#contact")}
           className="bg-slate-500 hover:bg-green-600 py-2 px-4 rounded text-sm"
         >
