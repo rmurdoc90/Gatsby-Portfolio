b2a47e8fb23e23b7b22c9f3a788c4f35ea67bd7b
rmurdoc90 <rmurdoc90@gmail.com>
2022-07-27 20:36:07 -0700

    EOD

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 function footer() {
-  return
-  ;<div className="bg-slate-700 h-12 p-4"></div>
+  return (
+    <div className="bg-slate-700 h-12 p-8">
+      <h1>Footer</h1>
+    </div>
+  )
 }
 
 export default footer
